ad08a3985f4037c0aacf4d91fdf05fb868591a43
Benoît <Benot@172.17.50.152>
2020-02-14 16:29:15 +0100

    Merge origin/master
    Conflicts:
    	src/hashcode/practice/HashcodePractice.java

--- src/hashcode/practice/Input.java
+++ src/hashcode/practice/Input.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
@@ -12,20 +13,20 @@ import java.util.ArrayList;
  */
 public class Input {
 
-    public static void input(Integer N, Integer M, ArrayList<Integer> Pizza) throws FileNotFoundException, IOException {
+    public static void input(Integer N, Integer M, ArrayList<Integer> Pizza,ArrayList<Integer> PizzaTri) throws FileNotFoundException, IOException {
         String S;
         int Length = 0;
         BufferedReader reader = new BufferedReader(new FileReader("a_example.in"));
         S = reader.readLine();
         M = Integer.valueOf(S.substring(0, S.indexOf(" ")));
         N = Integer.valueOf(S.substring(S.indexOf(" ") + 1, S.length()));
-        String T[]=new String[N];
+        String T[];
         S = reader.readLine();
-        System.out.println(S);
-        T=S.split(" ");
+        T = S.split(" ");
         for (int i = 0; i < N; i++) {
             Pizza.add(Integer.valueOf(T[i]));
-            System.out.println(Pizza.get(i));
         }
+        PizzaTri = Pizza;
+        Collections.sort(PizzaTri);
     }
 }
