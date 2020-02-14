17979bf546521ef47dc81fcaf11db1fddb85e23a
Taydo <leon.marteau.lm@gmail.com>
2020-02-14 16:59:53 +0100

    correction input

--- src/hashcode/practice/Input.java
+++ src/hashcode/practice/Input.java
@@ -6,27 +6,43 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Benoît
  */
 public class Input {
 
-    public static void input(Integer N, Integer M, ArrayList<Integer> Pizza,ArrayList<Integer> PizzaTri) throws FileNotFoundException, IOException {
-        String S;
-        int Length = 0;
-        BufferedReader reader = new BufferedReader(new FileReader("a_example.in"));
-        S = reader.readLine();
-        M = Integer.valueOf(S.substring(0, S.indexOf(" ")));
-        N = Integer.valueOf(S.substring(S.indexOf(" ") + 1, S.length()));
-        String T[];
-        S = reader.readLine();
-        T = S.split(" ");
-        for (int i = 0; i < N; i++) {
-            Pizza.add(Integer.valueOf(T[i]));
+    public static Pair input(ArrayList<Integer> Pizza,ArrayList<Integer> PizzaTri){
+        int N=-1,M=-1;
+        
+        BufferedReader reader = null;
+        try {
+            String S;
+            int Length = 0;
+            reader = new BufferedReader(new FileReader("a_example.in"));
+            S = reader.readLine();
+            M = Integer.valueOf(S.substring(0, S.indexOf(" ")));
+            N = Integer.valueOf(S.substring(S.indexOf(" ") + 1, S.length()));
+            String T[];
+            S = reader.readLine();
+            T = S.split(" ");
+            for (int i = 0; i < N; i++) {
+                Pizza.add(Integer.valueOf(T[i]));
+            }   PizzaTri = Pizza;
+            Collections.sort(PizzaTri);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Input.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Input.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Input.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        PizzaTri = Pizza;
-        Collections.sort(PizzaTri);
+        return new Pair(M,N);
     }
 }
