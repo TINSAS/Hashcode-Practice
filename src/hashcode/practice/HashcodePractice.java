2e1b91701f3c8184176f3291f8890848b8917f6b
Benoît <Benot@172.17.50.152>
2020-02-14 15:06:42 +0100

    Merge origin/master
    Conflicts:
    	src/hashcode/practice/HashcodePractice.java

--- src/hashcode/practice/HashcodePractice.java
+++ src/hashcode/practice/HashcodePractice.java
@@ -15,7 +15,6 @@ public class HashcodePractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         //HEY
         //devghjk
     }
