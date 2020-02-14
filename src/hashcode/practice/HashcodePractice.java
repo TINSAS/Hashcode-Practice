824a609e05f13f49214f553c94e5e240b6ff00e7
Benoît <Benot@172.17.50.152>
2020-02-14 16:11:58 +0100

    Merge origin/master
    Conflicts:
    	src/hashcode/practice/HashcodePractice.java

--- src/hashcode/practice/HashcodePractice.java
+++ src/hashcode/practice/HashcodePractice.java
@@ -15,8 +15,7 @@ public class HashcodePractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //HEY
-        //devghjk
+        
     }
     
 }
--- src/hashcode/practice/Input.java
+++ src/hashcode/practice/Input.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hashcode.practice;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
  * @author Benoît
  */
 public class Input {
-    
+
+    public static void input(Integer N, Integer M, ArrayList<Integer> Pizza) throws FileNotFoundException, IOException {
+        String S;
+        int Length = 0;
+        BufferedReader reader = new BufferedReader(new FileReader("a_example.in"));
+        S = reader.readLine();
+        M = Integer.valueOf(S.substring(0, S.indexOf(" ")));
+        N = Integer.valueOf(S.substring(S.indexOf(" ") + 1, S.length()));
+        String T[]=new String[N];
+        S = reader.readLine();
+        System.out.println(S);
+        T=S.split(" ");
+        for (int i = 0; i < N; i++) {
+            Pizza.add(Integer.valueOf(T[i]));
+            System.out.println(Pizza.get(i));
+        }
+    }
 }
