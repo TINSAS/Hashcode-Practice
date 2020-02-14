4eed44ca3b2b60d4e43f9674959913d3fd923318
Benoît <Benot@172.17.50.152>
2020-02-14 17:00:20 +0100

    Merge origin/master
    Conflicts:
    	src/hashcode/practice/HashcodePractice.java

--- src/hashcode/practice/Output.java
+++ src/hashcode/practice/Output.java
@@ -5,10 +5,22 @@
  */
 package hashcode.practice;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Benoît
  */
 public class Output {
+    public static void researchLastIndex(ArrayList<Integer> Pizza, int Index){
+        System.out.print(Pizza.indexOf(Index)+" ");
+        Pizza.set(Pizza.indexOf(Index), -1);
+    }
     
+    public static void output(ArrayList<Integer> Pizza,ArrayList<Integer> PizzaTri){
+        System.out.println(PizzaTri.size());
+        for (int i=0;i<PizzaTri.size();i++){
+            researchLastIndex(Pizza, PizzaTri.get(i));
+        }  
+    }
 }
